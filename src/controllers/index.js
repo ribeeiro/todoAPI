7bc57d50fa03aa55e72a3404988ffd43e02b0714
thiago ribeiro <thiagogr71@gmail.com>
2021-01-13 22:41:34 -0300

    finished todo crud

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -30,6 +30,7 @@ exports.putTodo = async (req, res, next) => {
   const { text } = req.body;
 
   if (!text) return res.status(400).json({ status: 400, message: 'Sintaxe incorreta' });
+  if (isNaN(taskId)) return res.status(400).json({ status: 400, message: 'O id tem que ser um numero' });
 
   const task = await knex('Todo').select('*').where({ id: taskId, user_id: userId }).first();
   if (!task) return res.status(404).json({ status: 404, message: 'Task nao encontrada' });
@@ -40,5 +41,24 @@ exports.putTodo = async (req, res, next) => {
   } catch (err) {
     console.log(err);
     res.status(500).json({ status: 500, message: 'Erro interno, tente novamente.' });
+  }
+};
+
+exports.deleteTodo = async (req, res, next) => {
+  const userId = req.id;
+  const taskId = req.params.id;
+
+  if (isNaN(taskId)) return res.status(400).json({ status: 400, message: 'O id tem que ser um numero' });
+
+  const task = await knex('Todo').select().where({ id: taskId, user_id: userId }).first();
+
+  if (!task) return res.status(404).json({ status: 404, message: 'Task nao encontrada' });
+
+  try {
+    await knex('Todo').where(task).del();
+    res.status(200).json({ status: 200, message: 'task deletada com sucesso' });
+  } catch (err) {
+    console.trace(err);
+    res.status(500).json({ status: 500, message: 'erro interno, tente novamente' });
   }
 };
