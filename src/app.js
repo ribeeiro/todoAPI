d1e281c58c1dd2877d841a9aae1c36c98e650359
Thiago Ribeiro <56800420+Ribeir0o@users.noreply.github.com>
2021-02-07 18:18:04 -0300

    Remove Comments

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const router = require('./router');
 require('./lib/cronjob');
 app.use(express.json());
-// TODO USE UUID INSTEAD OF ID
+
 app.use('/api/v1', router);
 
 module.exports = app;
