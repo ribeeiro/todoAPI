4c3fd85387112fdbcee839e6ae7a127736adfed2
Thiago <thiagogr71@gmail.com>
2021-07-27 14:36:52 -0300

    handle no token provided exception

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 exports.validateJWT = (req, res, next) => {
-  const token = req.headers.authorization.trim().split(' ')[1];
   try {
+    const token = req.headers.authorization.trim().split(' ')[1];
     const payload = jwt.verify(token, process.env.JWT_PUBLIC_KEY);
     req.id = payload.id;
     next();
