fba131766b0d7710588b30f8aca29c0ebd44d6ba
thiago <thiagogr71@gmail.com>
2022-11-30 17:48:03 -0300

    feat(api): enable cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const router = require('./router');
 require('./lib/cronjob');
 
+const corsOptions = {
+  origin: 'http://localhost:5173'
+};
 app.use(express.json());
 
-app.use('/api/v1', router);
+app.use('/api/v1', cors(corsOptions), router);
 
 module.exports = app;
