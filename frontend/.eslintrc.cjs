0fd6fb202ecfd774ae9115c72be11289ab184538
thiago <thiagogr71@gmail.com>
2022-11-30 09:13:34 -0300

    chore(lint): modify eslintrc rules

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -7,13 +7,25 @@ module.exports = {
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: './tsconfig.json'
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', '@typescript-eslint', 'react-hooks'],
+  root: true,
   rules: {
     'prettier/prettier': 'error',
     'no-shadow': 'off',
     'import/prefer-default-export': 'off',
-    semi: 0
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.tsx', 'ts'] }
+    ],
+    semi: 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/semi': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   }
 };
