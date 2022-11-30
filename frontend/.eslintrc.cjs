00b76bfe6f4ff7978ac5545e9ccefc1a9bb6933c
thiago <thiagogr71@gmail.com>
2022-11-29 22:03:28 -0300

    chore(frontend): create boilerplate

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-shadow': 'off',
+    'import/prefer-default-export': 'off',
+    semi: 0
+  }
+};
